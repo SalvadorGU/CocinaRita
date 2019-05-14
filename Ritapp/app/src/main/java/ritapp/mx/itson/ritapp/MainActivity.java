e2cf41a4e062790460de3c77e908f6ff08f220b2
SalvadorGU <usalvadorgomez@gmail.com>
2019-05-14 16:20:40 -0700

    up

--- Ritapp/app/src/main/java/ritapp/mx/itson/ritapp/MainActivity.java
+++ Ritapp/app/src/main/java/ritapp/mx/itson/ritapp/MainActivity.java
@@ -11,10 +11,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        //ghghh
+
     }
 }
 
