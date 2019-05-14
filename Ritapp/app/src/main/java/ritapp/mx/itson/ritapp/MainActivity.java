0da72f013e3e4cc4692b1f3ff7dd479f53691847
SalvadorGU <usalvadorgomez@gmail.com>
2019-05-14 16:06:29 -0700

    up

--- Ritapp/app/src/main/java/ritapp/mx/itson/ritapp/MainActivity.java
+++ Ritapp/app/src/main/java/ritapp/mx/itson/ritapp/MainActivity.java
@@ -2,18 +2,30 @@ package ritapp.mx.itson.ritapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnregistrar).setOnClickListener(this);
+
     }
+
+
+    public void onClick(final View v){
+
+
+
+    }
+
+
 }
 
 
